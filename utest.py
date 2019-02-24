479341f830e0f7d5baf2a0000de52ac1f9237cf3
imgcre <imgcre@outlook.com>
2019-02-24 20:16:27 +0800

    auto-commit

--- utest.py
+++ utest.py
@@ -23,6 +23,7 @@ def main():
         for test_func in __get_method_from(test_case,
           where=lambda func: func.__name__ not in base_class_func_names):
             print('found func:', test_func.__name__)
+            continue
         else:
             print('found nothing :)')
 
