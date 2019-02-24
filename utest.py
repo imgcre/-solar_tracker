b5cf73e4f233a93bd6091cf62b8688bfa2d3614b
imgcre <imgcre@outlook.com>
2019-02-24 19:30:46 +0800

    auto-commit

--- utest.py
+++ utest.py
@@ -1,27 +1,27 @@
 import sys
 
 
-# find all method in the inherited class
 class TestCase(object):
-    def __init__(self):
-        # do not write code here
-        # we use another file to contain the test code
-        # we can use callable function to detect if a object is a function
+    def assert_equal(self, a, b):
+        # raise error when cond is not satisfied
         pass
-    pass
 
 
+# your test suit file should be the main module
 def main():
-    # when this file is as
-    # we can use globals() to get all class
-    # seems that globals() can only get the variables that in its own module
-    # globals()'s return val is mutable
-
-    # TODO: get the module which called this function
-    # use dir() to get all attr from a module!
     main_module = __import__('__main__')
-    main_attr_iter = map(lambda attr_name: getattr(main_module, attr_name), dir(main_module))
-    print(main_attr_iter)
-    for test_case in filter(lambda attr: type(attr) is type and issubclass(attr, TestCase), main_attr_iter):
+    for test_case in __get_attr_form(main_module, where=lambda attr: type(attr) is type and issubclass(attr, TestCase)):
         print(test_case)
+        # check the target class
+        # create a instance for the class
+        # and call its all method by that instance
+        # use callable() to detect whether a attr is a function
+        # inst = test_case()
+        # test_case_attr_ittr = map(lambda attr_name: getattr(test_case, attr_name), dir(test_case))
 
+
+def __get_attr_form(obj, *, where=None):
+    attr_iter = map(lambda attr_name: getattr(obj, attr_name), dir(obj))
+    if where is not None and callable(where):
+        attr_iter = filter(where, attr_iter)
+    return attr_iter
