3347ba46dfe0f3808218ae91d4c76bb5d9074557
imgcre <imgcre@outlook.com>
2019-02-26 11:46:26 +0800

    auto-commit

--- boot.py
+++ boot.py
@@ -4,6 +4,6 @@
 import machine
 import pyb
 
-pyb.main('main.py') # main script to run after this one
+pyb.main('syncpri_test.py') # main script to run after this one
 #pyb.usb_mode('VCP+MSC') # act as a serial and a storage device
 #pyb.usb_mode('VCP+HID') # act as a serial device and a mouse
