21fcf91c1b67d9f4bf302c60062ac371b5a14cba
imgcre <imgcre@outlook.com>
2019-02-27 16:24:03 +0800

    auto-commit

--- highord.py
+++ highord.py
@@ -1,5 +1,14 @@
 
 def equals(a):
     def func(x):
-        return a == x
+        return __resolve(x, a)[0] == x
     return func
+
+
+def __resolve(x, *args):
+    args = list(args)
+    for i in range(len(args)):
+        while callable(args[i]):
+            args[i] = args[i](x)
+    return args
+    pass
