de644bb4b923cfeac2d0255b7189478831a4a1f4
imgcre <imgcre@outlook.com>
2019-02-28 16:28:30 +0800

    auto-commit

--- highord.py
+++ highord.py
@@ -42,6 +42,5 @@ def get_attr(item):
 def __resolve(x, *args):
     args = list(args)
     for i in range(len(args)):
-        while callable(args[i]):
-            args[i] = args[i](x)
+        args[i] = args[i](x)
     return args
