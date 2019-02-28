59497d6954a6ca6a0eed1b0a7e7a11669f11d592
imgcre <imgcre@outlook.com>
2019-02-28 15:19:49 +0800

    auto-commit

--- dsl.py
+++ dsl.py
@@ -17,10 +17,23 @@ class DSLBuilder:
     def __init__(self):
         self.__method_chain = []
 
+    def __call__(self, obj):
+        chain_len = len(self.__method_chain)
+        if chain_len:
+            func = self.__method_chain[chain_len - 1]
+            for i in range(chain_len - 1):
+                func = self.__method_chain[chain_len - i - 2](func)
+            return func(obj)
+        else:
+            return obj
+
     def __eq__(self, other):
         # return a closure that return self
         # forward the args to equals
         return self.__make_wrapper(equals(other), called=True)
+
+    def __ne__(self, other):
+        return self.__make_wrapper(not_(equals(other)), called=True)
 
     def __make_wrapper(self, func, *, called=False):
         if called:
