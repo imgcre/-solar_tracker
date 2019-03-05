deeebfe05584427b5ba7ab3937dabeba84ce9660
imgcre <imgcre@outlook.com>
2019-03-05 12:35:58 +0800

    auto-commit

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ from dsl import *
 from uinspect import *
 import highord
 
+class Test:
+    def __get__(self, instance, owner):
+        return 5
+        pass
+    pass
--- utilities.py
+++ utilities.py
@@ -19,9 +19,6 @@ class ObjLike:
 
         return func
 
-    def __str__(self):
-        return '233'
-
 
 class Indicator(pyb.LED):
     __nested_times = dict()
