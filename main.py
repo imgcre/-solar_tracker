9af3b5ae3751d0fa6e21d9589a20b570671cb1b1
imgcre <imgcre@outlook.com>
2019-03-09 04:19:57 +0800

    auto-commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyb import Pin, ExtInt, LED
 from cmemgr import map_to_thread
 
-@map_to_thread(lambda cb: ExtInt(Pin('X11'), ExtInt.IRQ_RISING, pyb.Pin.PULL_NONE))
+@map_to_thread(lambda cb: ExtInt(Pin('X11'), ExtInt.IRQ_RISING, pyb.Pin.PULL_NONE, cb))
 def rtc_second():
     LED(1).toggle()
 
