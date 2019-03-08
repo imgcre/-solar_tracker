bf671e25e9e2ccb3b638b40a7d39d08e12006312
imgcre <imgcre@outlook.com>
2019-03-09 04:52:11 +0800

    auto-commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ ds3231 = I2C(1, I2C.MASTER)
 @map_to_thread(partial(ExtInt)(Pin('X11'), ExtInt.IRQ_RISING, pyb.Pin.PULL_NONE))
 def rtc_second():
     # [秒, 分, 时, 星期, 日, 月, 年]
-    time_info = [(b & 0x0f) + (b & 0xf0) * 10 for b in ds3231.mem_read(7, 104, 0)]
+    time_info = [(b & 0x0f) + (b >> 4) * 10 for b in ds3231.mem_read(7, 104, 0)]
     print(time_info)
 
 
