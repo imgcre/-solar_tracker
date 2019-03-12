c6a50b0b94d53e44e854cee87552e784fb45bd36
imgcre <imgcre@outlook.com>
2019-03-12 15:19:53 +0800

    auto-commit

--- utilities_test.py
+++ utilities_test.py
@@ -22,7 +22,7 @@ class TestUtilities(utest.TestCase):
 
     @utest.cond(equals(2))
     def test_tween(self):
-        tween = Tween(init_val=0, target_val=255, expected_duration=1000)
+        tween = Tween(init_val=0, target_val=255, expected_duration=100)
         tween.on_updated = pyb.LED(4).intensity
         tween.on_completed = lambda: tween.set_target(255 if tween.cur_value == 0 else 0)
 
