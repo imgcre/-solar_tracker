ab0a854e70084b03a85c5fdb23aa2b99d2106479
imgcre <imgcre@outlook.com>
2019-03-17 21:30:48 +0800

    auto-commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import csv
 # 步距角=1.8° -> 200
 
 p = Pin('X1') # X1 has TIM2, CH1
-tim = Timer(2, freq=2000)
+tim = Timer(2, freq=20000)
 ch = tim.channel(1, Timer.PWM, pin=p)
 ch.pulse_width_percent(50)
 
