3f2370b8ac7d9cab45be78f0470cf0369ca2412e
imgcre <imgcre@outlook.com>
2019-03-19 12:30:49 +0800

    auto-commit

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class Stepper:
             for _ in range(abs(int(times))):
                 self.step(direction=times > 0, period=period)
         else:
-            print('stepped')
+            print('dir =', direction)
             self.dir_pin.value(direction)
             self.step_pin.on()
             delay(int(period / 2))
