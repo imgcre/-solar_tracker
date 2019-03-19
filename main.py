6b2fee11341a3bb2b56dd139c051420a77fc84f2
imgcre <imgcre@outlook.com>
2019-03-19 12:33:49 +0800

    auto-commit

--- main.py
+++ main.py
@@ -22,10 +22,11 @@ class Stepper:
     # 如果time是负数, 则反向旋转
     def step(self, times=None, *, period=10, direction=0):
         if times:
+            print('dir =', times)
             for _ in range(abs(int(times))):
                 self.step(direction=times > 0, period=period)
         else:
-            print('dir =', direction)
+
             self.dir_pin.value(direction)
             self.step_pin.on()
             delay(int(period / 2))
