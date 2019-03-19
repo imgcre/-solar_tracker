fb53b77c8e994c74b81d80467722ed7e1b9b0b80
imgcre <imgcre@outlook.com>
2019-03-19 18:04:09 +0800

    auto-commit

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ from utilities import *
 import sys
 import csv
 
-# 步距角=1.8° -> 200
-
-# p = Pin('X1')  # X1 has TIM2, CH1
-# tim = Timer(2, freq=6400)
-# ch = tim.channel(1, Timer.PWM, pin=p)
-# ch.pulse_width_percent(50)
+# 步距角=1.8° -> 20
 
 
 class Stepper:
---
+++ random_test.py
@@ -0,0 +1,6 @@
+from pyb import *
+
+# p = Pin('X1')  # X1 has TIM2, CH1
+# tim = Timer(2, freq=6400)
+# ch = tim.channel(1, Timer.PWM, pin=p)
+# ch.pulse_width_percent(50)
