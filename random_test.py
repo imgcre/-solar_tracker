b80284fb91ceaee993767ac994aab1fc8a1e990c
imgcre <imgcre@outlook.com>
2019-03-22 13:06:50 +0800

    auto-commit

--- random_test.py
+++ random_test.py
@@ -2,5 +2,5 @@ from pyb import *
 
 p = Pin('X1')  # X1 has TIM2, CH1
 tim = Timer(2, freq=400)
-ch = tim.channel(1, Timer.PWM, pin=p)
+ch = tim.channel(2, Timer.PWM, pin=p)
 ch.pulse_width_percent(50)
