8d377f00d5292e4b64ba81cdaf8d1204477fad06
imgcre <imgcre@outlook.com>
2019-03-23 22:07:29 +0800

    auto-commit

--- random_test.py
+++ random_test.py
@@ -4,7 +4,7 @@ ch = None
 def test1():
     global ch
     # 步进电机测试
-    p = Pin('X1')  # X1 has TIM2, CH1
+    p = Pin('X2')  # X1 has TIM2, CH1
     tim = Timer(2, freq=400)
     ch = tim.channel(2, Timer.PWM, pin=p)
     ch.pulse_width_percent(50)
