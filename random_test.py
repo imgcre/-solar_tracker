05024b91280cc6633a82930351948bb65600421c
imgcre <imgcre@outlook.com>
2019-03-24 21:29:28 +0800

    auto-commit

--- random_test.py
+++ random_test.py
@@ -3,6 +3,7 @@ from main import *
 
 
 def test1():
+    # 舵机测试
     LED(2).on()
     while True:
         for i in range(180):
@@ -13,11 +14,14 @@ def test1():
             delay(20)
 
 
-
 def test2():
-
+    # 步进电机测试 X2  X3
+    stepper = Stepper('X2', 'X3')
+    while True:
+        stepper.step()
+        pyb.delay(100)
     pass
 
 
 if __name__ == '__main__':
-    test1()
+    test2()
