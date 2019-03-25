37ba8eca6fad4a34208f55dd9e6d0a9e145777d2
imgcre <imgcre@outlook.com>
2019-03-25 22:19:56 +0800

    auto-commit

--- random_test.py
+++ random_test.py
@@ -15,8 +15,13 @@ def test1():
 
 
 def test2():
+    adc_x5 = ADC(Pin('X5'))
+    adc_x6 = ADC(Pin('X6'))
+    adc_x7 = ADC(Pin('X7'))
+    adc_x8 = ADC(Pin('X8'))
     # 步进电机测试 X2  X3
     # 舵机测试
+    # 四路ADC测试
     i = -90
     dir_ = False
     while True:
@@ -30,6 +35,7 @@ def test2():
             i += 1
             if i >= 90:
                 dir_ = True
+        print([adc_xx.read() for adc_xx in [adc_x5, adc_x6, adc_x7, adc_x8]])
         pyb.delay(100)
     pass
 
