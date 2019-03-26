eb13ef963bd4df1a51047d10a78a3f52a5609d42
imgcre <imgcre@outlook.com>
2019-03-26 18:19:45 +0800

    auto-commit

--- deco.py
+++ deco.py
@@ -1,15 +1,4 @@
-"""
-具备赋值和状态检查的AIP
-:param default: 默认AIP属性值, 首次赋值前的任何取值将返回此值, 缺省为None
-:param name: (string)存储AIP值的内部变量名, 缺省则使用默认名称
-:param require: (callback or callback-list)赋值AIP的合法条件, 缺省则无条件
-:param state_assume: (callback)访问AIP的合法条件, 缺省则无断言
-:returns: AIP装饰器
-:raises AssertError: 由各种原因导致的断言失败
-"""
 # require是函数元组, micropy不支持functools, 故可自定义私有字段名
-
-
 def aip(*, default=None, name=None, require=None, state_assume=None):
 	def decorator(func):
 		private_name = '__' + func.__name__ if name is None else name
