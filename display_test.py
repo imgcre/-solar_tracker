50d73c6894ca6dff284b970cc0574100634afd32
imgcre <imgcre@outlook.com>
2019-03-27 18:31:19 +0800

    auto-commit

--- display_test.py
+++ display_test.py
@@ -12,16 +12,17 @@ class TestOLED(utest.TestCase):
     @utest.cond(equals(1))
     def test_random_points(self):
         oled.clear()
-        # while True:
-        #    for i in range(100):
-        #        x = uos.urandom(1)[0] % 128
-        #        y = uos.urandom(1)[0] % 64
-        #        color = uos.urandom(1)[0] % 2
-        #        oled.draw_point(x, y, color, auto_submit=False)
-        #    oled.submit()
-        # pass
-        pass
-    pass
+        while True:
+            with oled.session:
+                for i in range(64):
+                    oled[63-i][i] = 0
+                for i in range(64):
+                    oled[i][i] = 1
+            with oled.session:
+                for i in range(64):
+                    oled[i][i] = 0
+                for i in range(64):
+                    oled[63-i][i] = 1
 
 
 if __name__ == '__main__':
