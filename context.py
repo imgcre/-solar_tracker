2a9aae14e25d38c429e75c43eac15719f2b5f55d
imgcre <imgcre@outlook.com>
2019-03-27 19:26:01 +0800

    auto-commit

--- context.py
+++ context.py
@@ -3,23 +3,22 @@ from utilities import ThreadLocalStorage, ObjLike
 
 class Context:
     def __init__(self, *, enter_func=None, exit_func=None):
-        self.__tls = ObjLike()  # ThreadLocalStorage()
-        self.__tls.nest_count = 0
+        self.__nest_count = 0
         self.__enter_func = enter_func
         self.__exit_func = exit_func
 
     def __enter__(self):
-        self.__tls.nest_count += 1
+        self.__nest_count += 1
         if self.__enter_func is not None:
             self.__enter_func()
 
     def __exit__(self, *args):
-        self.__tls.nest_count -= 1
+        self.__nest_count -= 1
         if self.__exit_func is not None:
             self.__exit_func()
 
     @property
     def nest_count(self):
-        return self.__tls.nest_count
+        return self.__nest_count
 
     pass
