c5381145c3792ff1789280878c82bb0032923b9b
imgcre <imgcre@outlook.com>
2019-03-27 19:04:08 +0800

    auto-commit

--- screen.py
+++ screen.py
@@ -1,6 +1,7 @@
 from display import OLED
 from font import *
 from utilities import Indexable
+from context import Context
 
 
 class Console(object):
@@ -9,6 +10,7 @@ class Console(object):
 		self.width = display.width // HALF_WIDTH_MIN_WIDTH
 		self.height = display.height // HALF_WIDTH_MIN_HEIGHT
 		self.__buffer = [[' ' for _ in range(self.height)] for _ in range(self.width)]
+		self.reverse = Context()
 		# 没有在上下文中, 则自动更新
 		pass
 
@@ -24,6 +26,8 @@ class Console(object):
 				for char in value:
 					for column in HALF_WIDTH_MIN[char]:
 						print(cur_page, cur_column)
+						if self.reverse.nest_count > 0:
+							column ^= 0xff
 						self.__display.draw_column(cur_page, cur_column, column)
 						cur_column += 1
 						if cur_column > self.width * HALF_WIDTH_MIN_WIDTH:
