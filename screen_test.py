315321d7b9cfd3644ceb65dba7a98f53cfa2a8e9
imgcre <imgcre@outlook.com>
2019-03-27 18:56:51 +0800

    auto-commit

--- screen_test.py
+++ screen_test.py
@@ -8,11 +8,10 @@ oled = OLED(I2C(2, mode=I2C.MASTER))
 console = Console(oled)
 oled.init()
 
-
 class TestConsole(utest.TestCase):
     def test_random_points(self):
         oled.clear()
-        pass
+        console[0][0] = 'Hello World!'
 
 
 if __name__ == '__main__':
