f510b38a2abc724093dfd140c4b634ebf0673850
imgcre <imgcre@outlook.com>
2019-03-31 21:05:14 +0800

    auto-commit

--- cmemgr.py
+++ cmemgr.py
@@ -19,7 +19,7 @@ class Mapper(object):
 
 	@classmethod
 	def __internal_thread(cls):
-		# _thread.stack_size(400)
+		_thread.stack_size(400)
 		while True:
 			syncpri.Event.wait_any(map(lambda m: m.__event, cls.__mappers))
 			for mapper in cls.__mappers:
