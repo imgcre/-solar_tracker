8319325fa0964818dddc35f96b9b69e96f692b36
imgcre <imgcre@outlook.com>
2019-03-31 21:50:55 +0800

    auto-commit

--- screen.py
+++ screen.py
@@ -28,7 +28,7 @@ class Console(object):
 						padding_char = kwargs['char']
 					except Exception:
 						pass
-					value = padding_char * (len(s) - args[0]) + s
+					s = padding_char * (len(s) - args[0]) + s
 				setter(key, s, flag=True)
 			else:
 				cur_page = item
