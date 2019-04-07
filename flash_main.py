860d3269312f7561d19c096d88e1a234bbd0d956
imgcre <imgcre@outlook.com>
2019-04-07 16:10:16 +0800

    auto-commit

---
+++ flash_main.py
@@ -0,0 +1,30 @@
+from utilities import *
+from pyb import *
+from cmemgr import Mapper
+
+servo_tween = Tween(max_speed=0.01,
+                    on_updated=Servo(1).angle)
+
+
+@key_handler('Y6')
+def key2():
+    print('key2 pressed')
+    servo_tween.set_target(-90, expected_duration=3000)
+    pass
+
+
+@key_handler('Y7')
+def key3():
+    print('key3 pressed')
+    servo_tween.set_target(0, expected_duration=3000)
+    pass
+
+
+@key_handler('Y8')
+def key4():
+    print('key4 pressed')
+    servo_tween.set_target(90, expected_duration=3000)
+
+
+Mapper.run(use_main_thread=True)
+
